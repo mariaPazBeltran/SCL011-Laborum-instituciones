0d321cb19a24a58d7ae661a5d37fd5f64819b638
GeryMolina <53873136+GeryMolina@users.noreply.github.com>
2020-02-10 11:37:44 -0300

    Estructura Login y Register (#5)
    * footer
    
    * Estructura de pantalla login y register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import './App.css';
-import Footer from './components/Footer';
+import Footer from './components/Footer'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
+      <Login/>
       <Footer/>
     </div>
   );
