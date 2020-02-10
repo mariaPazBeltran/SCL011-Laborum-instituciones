597aa38b5e4770b10f280bddf6cb5c9ed2e39e06
NataliaSaavedraM <nataliasaavedram@gmail.com>
2020-02-10 19:25:13 -0300

    configuracion de firebase y correccion de footer,js y css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import {useFirebaseApp} from 'reactfire';
 import Footer from './components/Footer'
 import Login from './components/Login'
 
 function App() {
+  const firebase=useFirebaseApp();
+  console.log(firebase);
   return (
     <div className="App">
       <Login/>
