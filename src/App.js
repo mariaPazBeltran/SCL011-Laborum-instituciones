1bece6cffb9708e15c4ba9c948bb4f099c87348f
NataliaSaavedraM <nataliasaavedram@gmail.com>
2020-02-18 13:36:10 -0300

    subir imagenes en la base de datos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-// import LoginScreen from './components/Login&register/ContenedorLogin'
-import RegisterScreen  from './components/Login&register/ContenedorRegister';
+ //import LoginScreen from './components/Login&register/ContenedorLogin'
+//import RegisterScreen  from './components/Login&register/ContenedorRegister';
 import ProviderContext from './states/provider';
 import {useFirebaseApp} from 'reactfire';
 //import Footer from './components/Footer';
 //import Header from './components/Header';
 //import Sidebar from './components/Sidebar';
-
+ import ProfileInstitution from '../src/views/ProfileInstitution'
+ 
 
 function App() {
   const firebase=useFirebaseApp();
@@ -15,9 +16,10 @@ function App() {
   return (
     <ProviderContext>
     <div className="App">
-      {/* <LoginScreen/> */}
-     <RegisterScreen/>
+      {/* <LoginScreen/>  */}
+     {/* <RegisterScreen/> */}
       {/* <Footer/> */}
+      <ProfileInstitution/> 
     </div>
     </ProviderContext>
   );
